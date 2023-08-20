dc063f81609336f9ce472a2b0efcd670a5c444ce
Alizarbukhari <Alizar.fsd@gmail.com>
2023-08-20 17:14:30 +0500

    assignment solved

---
+++ index.ts
@@ -0,0 +1,67 @@
+// let num:number = 1;
+// while (num <=10) {
+//     var result:number = num * 7
+//     console.log("7 *",num ,"=",result);
+//     num = num +1
+///////////                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
+// let index:number[]= [1,2,3,4,5,]
+// index.pop()
+// console.log(index);
+// var i:number =1 ;
+
+// //var result:number = 2 + 4+ i++;
+// var result:number = 2 + 4+ ++i;
+
+// console.log(i);
+// console.log(result);
+////pre fix pehly azafa hoga fr calculate kro
+////post fix pehly calclate kro fr azafa kro
+//for
+// var p:number = 50;
+// do{
+// console.log("naveed");
+// i++;
+// }while (i <= 30) 
+//write a ts program that calculate sum of even number between 1 to 100; 
+// let sum:number = 0;
+// for(let i = 1; i<=100; i++){
+// //console.log(i);
+
+// if(i % 2==0){
+// sum = sum +1 
+//     //console.log("even");
+// }
+// }
+ // console.log("sum",sum);
+
+
+let sum:number = 1;
+for (let i = 1; i <=100; i++) {
+    if (i%2 ==0) {
+        sum = sum +1
+console.log("i",i);
+   
+    }
+    
+}
+console.log("sum",sum);
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
